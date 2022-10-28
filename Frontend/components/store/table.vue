c7ec6d265cf9e8a834b0fc53e536560f2847c579
Poundpitak Amornlerdlittichai <Damon1fx@gmail.com>
2022-10-28 12:32:52 +0700

    update table

--- Frontend/components/store/table.vue
+++ Frontend/components/store/table.vue
@@ -1,5 +1,33 @@
 <template>
   <headerVue />
+  <div class="container justify-content-center col-8">
+    <div class="card">
+      <div class="card-body">
+        <button class="btn btn-primary col-auto mb-3 float-end">create</button>
+        <table class="table table-dark table-striped-columns">
+          <thead>
+            <tr class="text-center">
+              <th scope="col">#</th>
+              <th scope="col">First</th>
+              <th scope="col">Last</th>
+              <th scope="col">Manage</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr class="text-center">
+              <th scope="row">1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td class="col-4">
+                <button class="btn btn-warning mx-2">edit</button>
+                <button class="btn btn-danger">delete</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 import headerVue from "./header.vue";
--- Frontend/pages/store/index.vue
+++ Frontend/pages/store/index.vue
@@ -1,13 +1,16 @@
 <template>
   <NavbarVue />
+  <tableVue />
 </template>
 <script>
 import NavbarVue from "../../components/navbar.vue";
+import tableVue from "~~/components/store/table.vue";
 
 export default {
   name: "IndexVue",
   components: {
     NavbarVue,
+    tableVue,
   },
 };
 </script>
