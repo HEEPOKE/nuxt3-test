1dede18a030b066e683737e3947b8ca8a13cac5a
Poundpitak Amornlerdlittichai <Damon1fx@gmail.com>
2022-10-28 11:37:12 +0700

    update nav

--- Frontend/components/navbar.vue
+++ Frontend/components/navbar.vue
@@ -1,9 +1,7 @@
 <template lang="">
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
-      <NuxtLink to="/">
-        <a class="navbar-brand">Nuxt</a>
-      </NuxtLink>
+      <NuxtLink class="navbar-brand" to="/"> Nuxt </NuxtLink>
       <button
         class="navbar-toggler"
         type="button"
@@ -18,14 +16,12 @@
       <div class="collapse navbar-collapse" id="navbarText">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <NuxtLink to="/">
-              <a class="nav-link active" aria-current="page">Home</a>
+            <NuxtLink class="nav-link active" aria-current="page" to="/">
+              Home
             </NuxtLink>
           </li>
           <li class="nav-item">
-            <NuxtLink to="/store">
-              <a class="nav-link">Store</a>
-            </NuxtLink>
+            <NuxtLink to="/store" class="nav-link"> Store </NuxtLink>
           </li>
         </ul>
       </div>
