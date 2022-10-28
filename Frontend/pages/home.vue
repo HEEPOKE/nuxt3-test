033fd61f2dfcd10846ac02189732d646d6b8c386
Poundpitak Amornlerdlittichai <Damon1fx@gmail.com>
2022-10-28 10:32:25 +0700

    update pages

---
+++ Frontend/pages/home.vue
@@ -0,0 +1,14 @@
+<template>
+  <NavbarVue />
+  <div>home</div>
+</template>
+<script>
+import NavbarVue from "../components/navbar.vue";
+
+export default {
+  name: "HomeVue",
+  components: {
+    NavbarVue,
+  },
+};
+</script>
